9d7835516e72b42fb2ed11e63c166305118d9816
flutterrushikesh <flutterrushikesh@gmail.com>
2024-04-20 23:28:24 +0530

    Dependancy

--- app state/Proviḍer/my_provider/README.md
+++
@@ -1,3 +0,0 @@
-# my_provider
-
-A new Flutter project.
--- app state/Proviḍer/my_provider/android/app/src/main/kotlin/com/example/my_provider/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.my_provider
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
