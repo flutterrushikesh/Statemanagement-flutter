2675c761d94d2de88b19c72a3a558efe96ac8c70
flutterrushikesh <flutterrushikesh@gmail.com>
2024-05-08 00:40:47 +0530

    Dependacy

---
+++ app state/Provider/changeNotifierProxyProvider/player_example/lib/views/get_info_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class GetInfo extends StatefulWidget {
+  const GetInfo({super.key});
+
+  @override
+  State<GetInfo> createState() => _GetInfoState();
+}
+
+class _GetInfoState extends State<GetInfo> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
